3cce39d00125f73c3f92b22f142008804ef715ff
Kevin <kkemp@Kemp-home-PC.attlocal.net>
2021-02-16 17:09:23 -0600

    Fibonacci Sequence Done

--- Stanford106AAssignments/src/week2/Fibonacci.java
+++ Stanford106AAssignments/src/week2/Fibonacci.java
@@ -4,8 +4,27 @@ import acm.program.*;
 
 public class Fibonacci extends ConsoleProgram {
 
-	
-	public void run() {
+	private static final int MAX_TERM_VALUE = 10000;
 
+	public void run() {
+		
+		printFibSequence(MAX_TERM_VALUE);
+		
+	}
+	
+	private void printFibSequence(int largestNumber) {
+		
+		int fib1 = 0;
+		int fib2 = 1;
+		
+		println("This program lists the Fibonacci sequence.");
+		
+		while (fib1 < largestNumber) {
+			println(fib1);
+			
+			int fibSwap = fib1 + fib2;
+			fib1 = fib2;
+			fib2 = fibSwap;
+		}
 	}
 }
