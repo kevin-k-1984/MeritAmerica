e776999f7c77a5aa2eb5717e2003ee741b1fb11b
kevin_k <kkemp1984@gmail.com>
2021-02-19 16:06:25 -0600

    started drawing lines

--- Stanford106AAssignments/src/week2/DrawingLines.java
+++ Stanford106AAssignments/src/week2/DrawingLines.java
@@ -1,7 +1,38 @@
 package week2;
 
-import acm.program.GraphicsProgram;
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.event.*;
 
 public class DrawingLines extends GraphicsProgram {
 
+	private GLabel label;
+	
+	public void run() {
+		label = new GLabel("");
+		label.setFont("Times New Roman-36");
+		add(label, 50, 50);
+		
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		println("Hello World");
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+	
+	public void mouseMoved(MouseEvent e) {
+		label.setLabel("Mouse: (" + e.getX() + ", " + e.getY() + ")" );
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		
+	}
 }
