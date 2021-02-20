ff73ccd76857b2af5d64206b550dfd852cf61047
Kevin <kkemp@Kemp-home-PC.attlocal.net>
2021-02-19 19:22:42 -0600

    finished Drawing lines

--- Stanford106AAssignments/src/week2/DrawingLines.java
+++ Stanford106AAssignments/src/week2/DrawingLines.java
@@ -5,34 +5,29 @@ import acm.program.*;
 import java.awt.event.*;
 
 public class DrawingLines extends GraphicsProgram {
-
-	private GLabel label;
+	
+	private GLine lineObj;	
 	
 	public void run() {
-		label = new GLabel("");
-		label.setFont("Times New Roman-36");
-		add(label, 50, 50);
-		
 		addMouseListeners();
 	}
 	
-	public void mouseClicked(MouseEvent e) {
-		println("Hello World");
-	}
-	
 	public void mousePressed(MouseEvent e) {
-		
+		lineObj = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+		add(lineObj, e.getX(), e.getY());
 	}
 	
 	public void mouseReleased(MouseEvent e) {
-		
+		lineObj = null;
 	}
 	
 	public void mouseMoved(MouseEvent e) {
-		label.setLabel("Mouse: (" + e.getX() + ", " + e.getY() + ")" );
+//		label.setLabel("Mouse: (" + e.getX() + ", " + e.getY() + ")" );
 	}
 	
 	public void mouseDragged(MouseEvent e) {
-		
+		if (lineObj != null) {
+			lineObj.setEndPoint(e.getX(), e.getY());
+		}
 	}
 }
