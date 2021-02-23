5dcc8d46ef5a07dd3b9ca97c886371d6ba36a90b
Kevin <kkemp@Kemp-home-PC.attlocal.net>
2021-02-23 16:20:59 -0600

    DeleteCharactersFromString Done

--- Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
+++ Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
@@ -5,7 +5,16 @@ import acm.program.ConsoleProgram;
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
-
+		println(removeAllOccurrences("This is a test", 't'));
+		println(removeAllOccurrences("Summer is here!", 'e'));
+		println(removeAllOccurrences("---0---", '-'));
 	}
-	
+
+	private String removeAllOccurrences(String string, char c) {
+		String result = "";
+		for (char current: string.toCharArray()) {
+			if (current != c) { result += current;}
+		}
+		return result;
+	}
 }
