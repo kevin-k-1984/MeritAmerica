f69f3275cea75d0c4ebb19a873a9125e5d271173
Kevin Kemp <kkemp1984@gmail.com>
2021-03-02 10:02:41 -0600

    working on word count week 4

--- Stanford106AAssignments/src/week4/WordCount.java
+++ Stanford106AAssignments/src/week4/WordCount.java
@@ -1,11 +1,54 @@
 package week4;
 
+import java.io.*;
+
 import acm.program.ConsoleProgram;
+import acm.util.ErrorException;
 
 public class WordCount extends ConsoleProgram {
 	
 	public void run() {
-
+		println("File = " + "lear.txt");
+		println("Lines = " + this.numberOfLines("lear.txt"));
+		println("Words = " + this.numberOfWords());
+		println("Chars = " + this.numberOfCharacters());
 	}
 	
+	public int numberOfLines(String fileName) {
+		BufferedReader rd = null;
+		int count = 0;
+		
+		try {
+			rd = new BufferedReader(new FileReader(fileName));
+			while (true) {
+				if (rd.readLine() == null) { break; }
+				count++;
+			}
+			rd.close();
+		} catch (IOException e) {
+			throw new ErrorException(e);
+		}
+		
+		return count;
+	}
+	
+	public int numberOfWords() {
+//		try {
+//			BufferedReader rd = new BufferedReader(new FileReader(""));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		return 0;
+	}
+	
+	public int numberOfCharacters() {
+//		try {
+//			BufferedReader rd = new BufferedReader(new FileReader(""));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		return 0;
+	}
 }
