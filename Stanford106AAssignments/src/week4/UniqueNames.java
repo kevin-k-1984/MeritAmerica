a4663522eec0a7ac631d1fc4032e2a38a2e79bee
kevin_k <kkemp1984@gmail.com>
2021-03-02 12:47:59 -0600

    Finished UniqueNames

--- Stanford106AAssignments/src/week4/UniqueNames.java
+++ Stanford106AAssignments/src/week4/UniqueNames.java
@@ -1,11 +1,28 @@
 package week4;
 
 import acm.program.ConsoleProgram;
+import acmx.export.java.util.*;
 
 public class UniqueNames extends ConsoleProgram {
 	
+	ArrayList namesList;
+	Iterator  namesListItr;
+	
 	public void run() {
-
+		namesList = new ArrayList();
+		namesListItr = namesList.iterator();
+		
+		while (true) {
+			String name = readLine("Enter name: ");
+			if (name.equals("")) break;
+			if (!namesList.contains(name)) namesList.add(name);
+		}
+		
+		println("Unique name list contains:");
+		
+		while (namesListItr.hasNext()) {
+			println(namesListItr.next());
+		}
 	}
 	
 }
